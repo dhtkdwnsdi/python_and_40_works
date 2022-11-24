1dc6c08f7cdbf2914b4d0da8ff68aee0008e4c9c
osj950329 <osj950329@kdiwin.com>
2022-11-24 17:53:59 +0900

    2022-11-24 commit

---
+++ main2-3.py
@@ -0,0 +1,9 @@
+#컴퓨터의 외부 및 내부 IP 확인하기
+#컴퓨터 외부 IP 알아보는 코드 만들고 실해
+import requests
+import re
+
+req = requests.get("http://ipconfig.kr")
+out_addr = re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
+print(out_addr)
+
