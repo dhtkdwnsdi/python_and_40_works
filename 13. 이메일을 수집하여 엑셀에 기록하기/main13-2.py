507620c8fa381e01039cc852c9ed9d534910b48b
오상준 (IT개발그룹) <dhtkdwnsdi@gmail.com>
2022-11-30 17:44:50 +0900

    2022-11-30 커밋

---
+++ 13. 이메일을 수집하여 엑셀에 기록하기/main13-2.py
@@ -0,0 +1,18 @@
+#리스트에서 중복 내용 제거하는 코드 만들기
+# 이메일 형식을 추출하는 코드 만들기
+import re
+
+test_string = """
+aaa@bbb.com
+123@abc.co.kr
+test@hello.kr
+ok@ok.co.kr
+ok@ok.co.kr
+ok@ok.co.kr
+no.co.kr
+no.kr
+"""
+
+results = re.findall(r'[\w\.-]+@[\w\.-]+', test_string)
+results = list(set(results))
+print(results)
